73483e591d609328c4253f299ebf18a1e350accd
author-sanjay <sanjaykumar73189@gmail.com>
2022-02-15 23:12:21 +0530

    ADD: ImgSlider

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
+
+function Home() {
+  return (
+    <Container>
+        <ImgSlider/>
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 270px);
+  overflow-x: hidden;
+  display: block;
+  
+  padding: 0 calc(3.5vw + 5px);
+  &:before {
+    background: url("/images/home-background.png") center center / cover
+      repeat fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    left:0;
+    right:0;
+    bottom:0;
+    z-index:-1;
+    
+  }
+
+`
